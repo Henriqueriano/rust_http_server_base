787ce34b0aaa2c9f3fcb0c123d95e2a62e5fe845
Daniel Henrique <daniel.neves0021@gmail.com>
2025-06-12 13:51:54 -0300

    refactor: cargo fmt

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,16 +1,15 @@
-use std::net::{TcpStream, Shutdown};
-use std::io::{Write};
 use std::io;
-fn main() -> io::Result<()>
-{   
+use std::io::Write;
+use std::net::{Shutdown, TcpStream};
+fn main() -> io::Result<()> {
     //https://doc.rust-lang.org/std/io/#standard-input-and-output
-    loop
-    {
+    loop {
         let mut conn: TcpStream = TcpStream::connect("127.0.0.1:4343").expect("Failed!");
         let mut msg: String = String::new();
         println!("Type what do u want to send: ");
         io::stdin().read_line(&mut msg).unwrap();
         let _ = conn.write(msg.as_bytes());
-        conn.shutdown(Shutdown::Both).expect("Failed when shutdown!")
-    }   
+        conn.shutdown(Shutdown::Both)
+            .expect("Failed when shutdown!")
+    }
 }
